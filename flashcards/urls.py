e52c4bd485ed986c6c005a8b2c4b3a1d36892d52
sarahartsoe <sarahartsoe@gmail.com>
2021-06-25 17:27:38 -0400

    built registration

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name="home"),
+    path('accounts/', include('registration.backends.default.urls')),
 ]
--- flashcards/views.py
+++ flashcards/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Deck, Card
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def homepage(request):
-    pass
+    return render(request, "flashcards/homepage.html")
