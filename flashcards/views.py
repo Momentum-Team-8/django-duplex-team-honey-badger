8f592dd0f245a9801b7beffb27e3406ef1a3d75c
qbarefoot <quintenbarefoot@gmail.com>
2021-06-27 13:18:26 -0400

    add deck

--- flashcards/views.py
+++ flashcards/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Deck, Card, User
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
-
+from .forms import DeckForm
 # Create your views here.
 def homepage(request):
     return render(request, "flashcards/homepage.html")
@@ -16,3 +16,36 @@ def list_deck(request):
     decks = Deck.objects.all()
     return render(request, "flashcards/list_deck.html",
                   {"decks": decks})
+
+@login_required
+def add_deck(request):
+    if request.method == 'POST':
+        form = DeckForm(request.POST)
+        if form.is_valid():
+            deck = form.save(commit=False)
+            deck.created_date = timezone.now()
+            deck.save()
+            return redirect('list_deck')
+    else:
+        form = DeckForm()
+    return render(request, 'flashcards/add_deck.html', {'form': form})
+
+@login_required
+def edit_deck(request, pk):
+    decks = get_object_or_404(Deck, pk=pk)
+    if request.method == 'GET':
+        decks = DeckForm(instance=decks)
+    else:
+        decks = DeckForm(data=request.POST, instance=decks)
+        if decks.is_valid():
+            decks.save()
+            return redirect(to='list_deck')
+
+        return render(request, "flashcards/edit_deck.html", {'decks': decks})
+
+@login_required
+def delete_deck(request, pk):
+    decks = get_object_or_404(Deck, pk=pk)
+    if request.method == 'POST':
+        decks.delete()
+        return redirect('list_deck')
