0112b66b52a17bc497d9a0b94611a8c9d9166673
itsdeebarnett <jdeebarnett@gmail.com>
2021-06-30 10:40:38 -0400

    ??

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -8,22 +8,13 @@ urlpatterns = [
     path('', views.homepage, name="home"),
     path('accounts/', include('registration.backends.default.urls')),
     path('profile/', views.profile_page, name='profile_page'),
-<<<<<<< HEAD
-=======
     path('<int:pk>/delete_card/', views.delete_card, name='delete_card'),
     path('<int:pk>/edit_card/', views.edit_card, name='edit_card'),
->>>>>>> main
     path('decklist/', views.list_deck, name='list_deck'),
     path('decklist/add/', views.add_deck, name='add_deck'),
     path('decklist/<int:pk>/edit/', views.edit_deck, name='edit_deck'),
     path('decklist/<int:pk>/delete/', views.delete_deck, name='delete_deck'),
     path('decklist/<int:pk>/list_card', views.list_card, name='list_card'),
     path('decklist/<int:pk>/list_card/add_card', views.add_card, name='add_card'),
-<<<<<<< HEAD
-    path('decklist/<int:deck_pk>/list_card/<int:card_pk>/edit_card', views.edit_card, name='edit_card'),
-    path('decklist/<int:pk>/list_card/<int:pk>/delete_card', views.delete_card, name='delete_card'),
-
-=======
     path('card/<int:pk>/mark_correct', views.mark_correct, name='mark_correct')
->>>>>>> main
 ]
